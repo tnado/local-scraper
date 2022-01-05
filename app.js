00dbefc47211e204b4079992dbcc34e846136e0d
tnado <steinlagems@gmail.com>
2022-01-05 13:15:57 -0600

    problem with loop to find potential hidden class.

--- app.js
+++ app.js
@@ -18,35 +18,39 @@ async function scrapeData() {
             
             const company = { profile_page_url: "", title: "", image_url: "", website: "", description: "" };
 
-            company.profile_page_url = $(el).children("a").attr("href");
+            // company.profile_page_url = $(el).children("a").attr("href"); //Working
 
-            company.title = $(el).children("h3").text().trim();
+            // company.title = $(el).children("h3").text().trim(); //Working
 
-            // company.image_url = $(el).children(".img").attr("src");
+            // company.image_url = $(el).children(".img").attr("src"); //old doesn't work
 
-            company.image_url = $(el).find('img')[0].attribs['data-src'];
+            // company.image_url = $(el).find('img')[0].attribs['data-src']; //Working
             //.find looks for all img and makes an array
             //[0] is the index selector of it
             //.attribs is object attributes raw JS
             //['data--src'] is the hidden class
 
-            // company.website = $(el).children('.website-link__item').attrib('href');
+            // company.website = $(el).find('a').length //testing
             //company website scrape not working atm. plus the url would need to be cleaned up.
 
-            company.description = $(el).children("p").text();
+            // company.description = $(el).children("p").text(); //Working
 
+            company.wesbite = $(el).find('a').each( item, index => { console.log(`${index} - ${item}`) })
+            
             companies.push(company);
         });
 
-        console.dir(companies);
+        // console.log(companies);
 
-        fs.writeFile("companies.json", JSON.stringify(companies, null, 2), (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("successfully written data to file");
-        });
+//         console.dir(companies);
+
+//         fs.writeFile("companies.json", JSON.stringify(companies, null, 2), (err) => {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             }
+//             console.log("successfully written data to file");
+//         });
     } catch (err) {
         console.error(err);
     }
